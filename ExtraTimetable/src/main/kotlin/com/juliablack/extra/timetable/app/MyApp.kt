9bc7164f3775c0d6374ed9fac6fedec2f3504e58
julia-black <b-r-cat@ya.ru>
2019-05-11 17:15:33 +0400

    Add icon

--- ExtraTimetable/src/main/kotlin/com/juliablack/extra/timetable/app/MyApp.kt
+++ ExtraTimetable/src/main/kotlin/com/juliablack/extra/timetable/app/MyApp.kt
@@ -2,18 +2,23 @@ package com.juliablack.extra.timetable.app
 
 import com.juliablack.extra.timetable.logic.GeneratorTimeTable
 import com.juliablack.extra.timetable.view.MainView
+import javafx.scene.image.Image
 import tornadofx.*
-
+import java.io.File
 
 class MyApp : App(MainView::class, Styles::class) {
 
     init {
-        val generatorTimeTable = GeneratorTimeTable()
-        generatorTimeTable.maxLessonsOfDay = MAX_LESSONS_OF_DAY
-        generatorTimeTable.optionalLessonsOfDay = OPTIONAL_LESSONS_OF_DAY
-        generatorTimeTable.generateStartPopulation(COUNT_OF_POPULATION)
-        generatorTimeTable.generateTimetable()
+       addStageIcon(Image(File("src/main/resources/app/timetable.png").toURI().toString()))
+
+       val generatorTimeTable = GeneratorTimeTable()
+       generatorTimeTable.maxLessonsOfDay = MAX_LESSONS_OF_DAY
+       generatorTimeTable.optionalLessonsOfDay = OPTIONAL_LESSONS_OF_DAY
+       generatorTimeTable.generateStartPopulation(COUNT_OF_POPULATION)
+       generatorTimeTable.generateTimetable()
     }
+
+
 
     companion object {
         const val COUNT_OF_POPULATION = 1
